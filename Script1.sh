a507e21e2c384a7a24a171e10464a8e8a30ce519
jstdevesh <deveshjoshi70@gmail.com>
2025-05-16 11:25:24 +0530

    commited

--- Script1.sh
+++ Script1.sh
@@ -1,7 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
-echo "We are learning Git and GitHub"
-echo "How are you?"
-echo "We are learning git"
-echo "we are learning branching"
+echo "we are learning git"
